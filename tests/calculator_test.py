fcf8471a55d2fae9d5a98aefa271d768cfb3b7b0
Illya Sokolov <illya.sokolov82@gmail.com>
2020-12-13 22:12:24 +0200

    subtraction added

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -12,6 +12,9 @@ class TestCalc:
     """def test_multiply_calculate_failed(self):
         assert self.calc.multiply(self, 2, 2) == 5"""
 
+    def test_subtraction_calculate_correctly(self):
+        assert self.calc.subtraction(self, 13, 4) == 9
+
     def test_adding_calculate_correctly(self):
         assert self.calc.adding(self, 3, 8) == 11
 
