bf9050f841f1289a8599f6c44dc9ae3757de79cd
Illya Sokolov <illya.sokolov82@gmail.com>
2020-12-13 22:00:32 +0200

    initial commit

---
+++ tests/calculator_test.py
@@ -0,0 +1,22 @@
+import pytest
+from app.calculator import Calculator
+
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    """def test_multiply_calculate_failed(self):
+        assert self.calc.multiply(self, 2, 2) == 5"""
+
+    def test_adding_calculate_correctly(self):
+        assert self.calc.adding(self, 3, 8) == 11
+
+    def test_division_calculate_correctly(self):
+        assert self.calc.division(self, 10, 4) == 2.5
+
+    def test_modulo_calculate_correctly(self):
+        assert self.calc.modulo(self, 10, 4) == 2
